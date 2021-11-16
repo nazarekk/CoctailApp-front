1c5aa9e8cd110b50149a12f02922db2a1fc2b249
Bohdan Sydorenko <sidorenko2002bogdan@gmail.com>
2021-11-17 00:53:09 +0200

    new login with token and logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,48 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private _rootUrl = "https://coctailapp.herokuapp.com"
+  private rootUrl = "http://localhost:8080"
+  private token = null
+
   constructor(private http: HttpClient) { }
 
   registerUser(user){
-    return this.http.post<any>(this._rootUrl + '/users', user)
+    return this.http.post<any>(this.rootUrl + '/users', user)
   }
 
-  loginUser(user){
-    return this.http.post<any>(this._rootUrl + '/login', user)
+  loginUser(user): Observable<{token: string}> {
+    return this.http.post<{token: string}>(this.rootUrl + '/login', user)
+      .pipe(
+        tap(
+          ({token}) => {
+            localStorage.setItem('auth-token', token)
+            this.setToken(token)
+          }
+        )
+      )
   }
 
+  setToken(token: string){
+    this.token = token
+  }
+
+  getToken(): string{
+    return this.token
+  }
+
+  isAuthenticated(): boolean{
+    return !!this.token
+  }
+
+  logout(){
+    this.setToken(null)
+    localStorage.clear()
+  }
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,29 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 import { ConfirmedValidator } from './confirmed.validator';
 import {AuthService} from "../auth/auth.service";
+import {Router, ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
 
   title = 'Sign Up'
 
   form: FormGroup = new FormGroup({});
+  success = false
 
-  constructor(private fb: FormBuilder, private auth: AuthService) {
+  constructor(private fb: FormBuilder,
+              private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {}
 
-    this.form = fb.group({
+
+  ngOnInit(){
+    this.form = this.fb.group({
       email: ['', [Validators.required]],
       password: ['', [Validators.required]],
       doubleCheckPass: ['', [Validators.required]]
     }, {
       validator: ConfirmedValidator('password', 'doubleCheckPass')
     })
+
+    /*this.route.queryParams.subscribe((params:Params)=>{
+      if (params['user created successfully!']){
+        this.success = true;
+      }
+      console.log(this.success)
+    })*/
   }
 
   removeDoubleCheckPass(value:any){
@@ -32,7 +46,8 @@ export class RegistrationComponent {
   }
 
   submit(){
-    this.auth.registerUser(this.removeDoubleCheckPass(this.form.value))
+    console.log(this.removeDoubleCheckPass(this.removeDoubleCheckPass(this.form.value)))
+    this.auth.registerUser(this.form.value)
       .subscribe(
         res=>console.log(res),
         err=>console.log(err)
