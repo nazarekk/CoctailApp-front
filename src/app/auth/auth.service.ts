19c6796a8ad125d3c9f45bd7c307bf8391303022
nazarekk <01102000nz@gmail.com>
2021-11-17 00:37:35 +0200

    Tets

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,15 +6,15 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AuthService {
 
-  private _rootUrl = "https://coctailapp.herokuapp.com"
+  private _rootUrl = "http://localhost:8080"
   constructor(private http: HttpClient) { }
 
   registerUser(user){
-    return this.http.post<any>(this._rootUrl + '/users', user)
+    return this.http.post<any>(this._rootUrl + '/api/users', user)
   }
 
   loginUser(user){
-    return this.http.post<any>(this._rootUrl + '/login', user)
+    return this.http.post<any>(this._rootUrl + '/api/auth/login', user)
   }
 
 }
