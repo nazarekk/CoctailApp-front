192d564ade6e64b6494ee99ef9b45893ccd202a0
nazarekk <01102000nz@gmail.com>
2021-11-17 20:50:56 +0200

    deploy fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,9 @@ import {AuthService} from "./auth/auth.service";
 import { LoginComponent } from './login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {ModeratorComponent} from "./moderatorRegistration/moderator.component";
-import {MatButtonModule} from "@angular/material/button";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatNativeDateModule} from "@angular/material/core";
 import {ModeratorVerificationComponent} from "./moderatorVerification/moderator.verification.component";
 import {ModeratorPersonalComponent} from "./moderatorPersonal/moderator.personal.component";
-
-const MaterialComponents = [
-  MatButtonModule,
-  MatButtonToggleModule
-];
 
 @NgModule({
   declarations: [
@@ -37,10 +29,7 @@ const MaterialComponents = [
     HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    MatNativeDateModule,
-    MaterialComponents
   ],
-  exports: [MaterialComponents],
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
